687e53abe8988a6f49421bc16cd71797545904de
Eric Bandera <ericbandera@gmail.com>
2023-04-17 15:17:37 -0500

    incorporated failing test 9

---
+++ Server/firebaseConfig.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = () => {
+    return {
+        apiKey: "AIzaSyBjzmfmtsQHUQgK9naYd1kDgXGVfRS0PsY",
+        authDomain: "badbank-89224.firebaseapp.com",
+        projectId: "badbank-89224",
+        storageBucket: "badbank-89224.appspot.com",
+        messagingSenderId: "132338591308",
+        appId: "1:132338591308:web:93a46de4f0ed85e0ca76e9"
+    }
+};
+ module.exports = firebaseConfig;
+
+// Initialize Firebase
+
